2440a73c77b6e436b487296419976d73b3f939a6
Barry Lee <blee@gala.games>
2022-08-02 11:09:05 -0600

    Modified curriculum to hybrid model

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerBallController_eval.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerBallController_eval.cs
@@ -1,32 +1,32 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class SoccerBallController : MonoBehaviour
-{
-    public GameObject area;
-    [HideInInspector]
-    public SoccerEnvController envController;
-    public string purpleGoalTag; //will be used to check if collided with purple goal
-    public string blueGoalTag; //will be used to check if collided with blue goal
+// public class SoccerBallController : MonoBehaviour
+// {
+//     public GameObject area;
+//     [HideInInspector]
+//     public SoccerEnvController envController;
+//     public string purpleGoalTag; //will be used to check if collided with purple goal
+//     public string blueGoalTag; //will be used to check if collided with blue goal
 
-    void Start()
-    {
-        Debug.Log("PlayerLog: Ball position is: " + transform.position);
-        envController = area.GetComponent<SoccerEnvController>();
-    }
+//     void Start()
+//     {
+//         Debug.Log("PlayerLog: Ball position is: " + transform.position);
+//         envController = area.GetComponent<SoccerEnvController>();
+//     }
 
     
-    void OnCollisionEnter(Collision col)
-    {
-        Debug.Log("PlayerLog: Ball position is: " + transform.position);
-        if (col.gameObject.CompareTag(purpleGoalTag)) //ball touched purple goal
-        {
-            Debug.Log("PlayerLog: Blue Team Scored!");
-            envController.GoalTouched(Team.Blue);
-        }
-        if (col.gameObject.CompareTag(blueGoalTag)) //ball touched blue goal
-        {
-            Debug.Log("PlayerLog: Purple Team Scored!");
-            envController.GoalTouched(Team.Purple);
-        }
-    }
-}
+//     void OnCollisionEnter(Collision col)
+//     {
+//         Debug.Log("PlayerLog: Ball position is: " + transform.position);
+//         if (col.gameObject.CompareTag(purpleGoalTag)) //ball touched purple goal
+//         {
+//             Debug.Log("PlayerLog: Blue Team Scored!");
+//             envController.GoalTouched(Team.Blue);
+//         }
+//         if (col.gameObject.CompareTag(blueGoalTag)) //ball touched blue goal
+//         {
+//             Debug.Log("PlayerLog: Purple Team Scored!");
+//             envController.GoalTouched(Team.Purple);
+//         }
+//     }
+// }
